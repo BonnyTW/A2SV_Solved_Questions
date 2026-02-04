93ed2012b6805b239a305aea48e9a9e088616e45
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-02-04 17:55:24 +0300

    check integer coverage
    Implement a method to check if all integers in a specified range are covered by given ranges.

---
+++ 1893. Check if All the Integers in a Range Are Covered.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        notall=False
+        for i in range(left,right+1):
+            j=0
+            while j<len(ranges) and not(ranges[j][0]<=i<=ranges[j][1]):
+                j+=1
+            if j>=len(ranges):
+                return False
+        return True
+            
+
+
+        
