a198df91e24ee72a35c5d59b1d8361e654b6c68a
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-02-05 16:28:15 +0300

    Fix character counting logic in word formation

---
+++ 1160. Find Words That Can Be Formed by Characters.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        ans=0
+        charCounter=Counter(chars)
+        for word in words:
+            myd=Counter(word)
+            for k in myd:
+                if k in charCounter:
+                    if myd[k]<=charCounter[k]:
+                        continue
+                    else:
+                        break
+                else:
+                    break
+            else:
+                ans+=len(word)
+        return (ans)
+                
+        
