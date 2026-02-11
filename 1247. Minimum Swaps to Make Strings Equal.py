62badce096549ba952b102994db465711b2dd9bf
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-02-11 23:55:56 +0300

    Create 1247. Minimum Swaps to Make Strings Equal.py

---
+++ 1247. Minimum Swaps to Make Strings Equal.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minimumSwap(self, s1: str, s2: str) -> int:
+
+        my_dict=Counter(s1+s2)
+        for num in my_dict.values():
+            if num%2:
+                return -1
+
+        count=Counter()
+        for i in range(len(s1)):
+            if s1[i]!=s2[i]:
+                count[s1[i]+s2[i]]+=1 
+        res=0
+        for value in count.values():
+            if value%2:
+                res+=((value//2)+1)
+            else:
+                res+=(value//2)
+        return res
+        
+
+        
+
+        
