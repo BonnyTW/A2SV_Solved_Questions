58002d13f21750eb9331ecf894fd89bfb3499b52
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-02-19 00:14:55 +0300

    Update 1365. How Many Numbers Are Smaller Than the Current Number.py

--- 1365. How Many Numbers Are Smaller Than the Current Number.py
+++ 1365. How Many Numbers Are Smaller Than the Current Number.py
@@ -1,16 +1,29 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        # counting sort
+        nums2 = nums[:]
+        max_num=max(nums2)
+        counting=[0]*(max_num+1)
+
+        for num in nums2:
+            counting[num]+=1
+
+        target=0
+
+        for idx,val in enumerate(counting):
+            for i in range(val):
+                nums2[target]=idx
+                target+=1
+        print(nums2)
+        
+
+        mydict={}
+        for i in range(len(nums2)):
+            if nums2[i] not in mydict:
+                mydict[nums2[i]]=i
+        print(mydict)
         ans=[]
         for i in range(len(nums)):
-            count=0
-            for j in range(len(nums)):
-                if i==j:
-                    continue
-                if nums[i]>nums[j]:
-                    count+=1
-            ans.append(count)
+            ans.append(mydict[nums[i]])
         return ans
-                
-
-
         
