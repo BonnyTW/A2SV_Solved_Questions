0502b25b3327ec889951272d497d9bccc53d2aba
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-02-20 17:07:57 +0300

    Update 229. Majority Element II.py

--- 229. Majority Element II.py
+++ 229. Majority Element II.py
@@ -1,10 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         count=Counter(nums)
-        ans=[]
-        for key,value in count.items():
-            if value>(len(nums)//3):
-                ans.append(key)
-        return ans
-
+        res=[]
+        for key in count:
+            if count[key]>(len(nums)/3):
+                res.append(key)
+        return res
+            
         
