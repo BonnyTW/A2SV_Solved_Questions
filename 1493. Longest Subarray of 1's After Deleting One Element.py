cd47c680c9dabc328bb5866e4991d50ad89ea945
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-02-26 20:47:07 +0300

    Add solution for longest subarray of 1's problem
    Implement a solution to find the longest subarray of 1's after deleting one element.

---
+++ 1493. Longest Subarray of 1's After Deleting One Element.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        left=0
+        right=0
+
+        zeroCount=0
+        maximum=0
+        while right< len(nums):
+            if nums[right]==0:
+                zeroCount+=1
+            
+            while zeroCount>1:
+                if nums[left]==0:
+                    zeroCount-=1
+                left+=1
+            maximum=max(maximum,right-left)
+            right+=1
+        return maximum
