0f7b8ebdc4179e34c990cfc1925794659e7eae7d
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-03-04 20:15:05 +0300

    Add solution for Maximum Sum Obtained of Any Permutation

---
+++ leetcode/1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
+        # nums = [1,2,3,4,5], requests = [[1,3],[0,1]]
+        # [1,1,-1,0,-1,0]->[1,2,1,1,0,0] -> [2,1,1,1,0]*[5,4,3,2,1]->[10,4,3,2,0]=19
+
+        psum=[0]*(len(nums)+1)
+
+        for s,e in requests:
+            psum[s] +=1
+            psum[e+1]-=1
+
+        for i in range(1,len(psum)):
+            psum[i] += psum[i-1]
+
+        print(psum)
+        nums.sort(reverse=True)
+        psum.sort(reverse=True)
+        print(psum)
+        
+        for i in range(len(nums)):
+            psum[i]*=nums[i]
+
+        return sum(psum)% (10**9 + 7)
+
+        
+
+        
