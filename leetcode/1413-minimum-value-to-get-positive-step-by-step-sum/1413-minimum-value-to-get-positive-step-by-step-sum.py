ee1af727837943c14a2a83180927d625516001ee
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-03-04 18:09:37 +0300

    Add solution for Minimum Value to Get Positive Step by Step Sum

--- leetcode/1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.py
+++ leetcode/1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.py
@@ -3,13 +3,15 @@ class Solution:
         # [-3,2,-3,4,2] --> [-3,-1,-4,0,2]
         # [1,2]->[1,3]
 
-        psum=[nums[0]]
 
-        for num in nums[1:]:
-            psum.append(psum[-1]+num)
-        print(psum)
-        
-        min_num=min(psum)
+        psum=0
+        min_num=float('inf')
+        for num in nums:
+            psum+=num
+            min_num=min(min_num,psum)
+
+
+    
         if min_num>0:
             return 1
         else:
