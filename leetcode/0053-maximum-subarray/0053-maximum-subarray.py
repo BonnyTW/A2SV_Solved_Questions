79acdcfd47e5cc6d202f44a1f3ba248c29371e38
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-03-06 18:50:44 +0300

    Add solution for Maximum Subarray

---
+++ leetcode/0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        #[-2,1,-3,4,-1,2,1,-5,4] [3,6]
+        #[0,-2,-1,-4,0,-1,1,2,-3,1]=2-(-4)=6
+
+        #[5,4,-1,7,8]
+        #[0,5,9,8,15,23]
+
+        #[0,-1]->[0,-1]
+
+        prefix=[0]
+        for num in nums:
+            prefix.append(prefix[-1]+num)
+        print(prefix)
+
+        largest=float('-inf')
+        minnum=float('inf')
+
+        for num in prefix:
+            largest=max(largest,num-(minnum))
+            if num < minnum:
+                minnum=num
+            
+            
+        return (largest) 
+                
+
+
+
+
+        
