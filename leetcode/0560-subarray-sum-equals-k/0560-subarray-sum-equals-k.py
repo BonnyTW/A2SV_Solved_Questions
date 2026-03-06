c0bd8e86b75fea2d44188e32251bdd8b3acc7bd3
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-03-06 18:18:34 +0300

    Add solution for Subarray Sum Equals K

--- leetcode/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ leetcode/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -10,17 +10,15 @@ class Solution:
         #{0:1,1:1,3:1,6:1,9:1}
         #count=3
 
-        my_dict=Counter()
+        my_dict=Counter({0:1})
 
-        prefix=[0]
-        for num in nums:
-            prefix.append(prefix[-1]+num)
-        print(prefix)
+        psum=0
         count=0
-        for num in prefix:
-            if num-k in my_dict:
-                count+=my_dict[num-k]
-            my_dict[num]+=1
+        for num in nums:
+            psum+=num
+            if psum-k in my_dict:
+                count+=my_dict[psum-k]
+            my_dict[psum]+=1
         print(my_dict)
         return count
             
