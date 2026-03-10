c76b7dc1a11671b11f1b0272fed317fe1dedefc5
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-03-10 11:22:55 +0300

    Add solution for Backspace String Compare

---
+++ leetcode/0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -0,0 +1,20 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        stack1=[]
+        stack2=[]
+
+        for ch in s:
+            if ch!='#':
+                stack1.append(ch)
+            else:
+                if stack1:
+                    stack1.pop()
+        for ch in t:
+            if ch!='#':
+                stack2.append(ch)
+            else:
+                if stack2:
+                    stack2.pop()
+        return stack1 == stack2
+        
+        
