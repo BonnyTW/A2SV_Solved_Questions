de1fed1ddb49340cb57ec658636f5820d5331c77
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-03-19 20:28:05 +0300

    Add solution for Sum of Nodes with Even-Valued Grandparent

---
+++ leetcode/1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumEvenGrandparent(self, root: Optional[TreeNode]) -> int:
+        self.ans=0
+
+        def helper(root,parent, grandpa):
+            if not root:
+                return 
+            if grandpa and grandpa.val % 2 == 0:
+                self.ans += root.val
+            
+            helper(root.left,root,parent)
+            helper(root.right,root,parent)
+            
+        helper(root,None,None)
+        return self.ans
+        
+            
+        
