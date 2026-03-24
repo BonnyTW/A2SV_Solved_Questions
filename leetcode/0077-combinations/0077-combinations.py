662af65eae04fb97b8cc2e102121b99e6257defb
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-03-24 20:13:51 +0300

    Add solution for Combinations

---
+++ leetcode/0077-combinations/0077-combinations.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        arr=[]
+        res=[]
+        
+        
+        def helper(n,l):
+            
+            
+            if len(arr) == k:
+                res.append(arr[:])
+                return
+            if  n>l:
+                return
+            arr.append(n)
+            helper(n+1,l)
+            arr.pop()
+            helper(n+1,l)
+            
+        helper(1,n)
+
+        return res
+            
+
+            
+
+        
