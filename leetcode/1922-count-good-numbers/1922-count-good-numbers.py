474e085f8686782ada43cc492c8cbf1c550c3aae
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-03-30 21:28:13 +0300

    Add solution for Count Good Numbers

--- leetcode/1922-count-good-numbers/1922-count-good-numbers.py
+++ leetcode/1922-count-good-numbers/1922-count-good-numbers.py
@@ -2,6 +2,7 @@ class Solution:
     def countGoodNumbers(self, n: int) -> int:
         n_e=(n+1)//2
         n_o=n//2
+        
 
         return (pow(5,n_e,(10**9)+7)*pow(4,n_o,(10**9)+7))%((10**9)+7)
 
