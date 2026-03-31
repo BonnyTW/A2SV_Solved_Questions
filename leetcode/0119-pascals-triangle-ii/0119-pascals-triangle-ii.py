a189fa438c4a2ef77bd0d2bb1d1bfdf4f958cafd
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-03-31 09:27:17 +0300

    Add solution for Pascal's Triangle II

--- leetcode/0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ leetcode/0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,17 +1,20 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-
         def helper(n):
             if n==0:
                 return [1]
             prev=helper(n-1)
-            curr=[1]
+            cur=[1]
             for i in range(1,len(prev)):
-                curr.append(prev[i-1]+prev[i])
-            curr.append(1)
-            return curr
+                cur.append(prev[i-1]+prev[i])
+            cur.append(1)
+            return cur
         return helper(rowIndex)
-            
+
+
+
+
+
         
         
         
