a9549700422249b163bf5e9bf0eed39507ef9088
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-03-31 09:43:05 +0300

    Add solution for Merge Two Sorted Lists

--- leetcode/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ leetcode/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,43 +5,49 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+
         arr1=[]
         arr2=[]
 
-        pointer1=list1
-        while pointer1 and pointer1.next:
-            arr1.append(pointer1.val)
-            pointer1=pointer1.next
-        if pointer1:
-            arr1.append(pointer1.val)
+        p1=list1
 
-        pointer2=list2
-        while pointer2 and pointer2.next:
-            arr2.append(pointer2.val)
-            pointer2=pointer2.next
-        if pointer2:
-            arr2.append(pointer2.val)
+        while p1 and p1.next:
+            arr1.append(p1.val)
+            p1=p1.next
+        if p1:
+            arr1.append(p1.val)
         
-        i=0
-        j=0
+        print(arr1)
 
-        ans=[]
-        while i < len(arr1) and j< len(arr2):
+        p2=list2
+
+        while p2 and p2.next:
+            arr2.append(p2.val)
+            p2=p2.next
+        if p2:
+            arr2.append(p2.val)
+        print(arr2)
+
+        i,j=0,0
+        res=[]
+        while i<len(arr1) and j< len(arr2):
             if arr1[i]<=arr2[j]:
-                ans.append(arr1[i])
+                res.append(arr1[i])
                 i+=1
             else:
-                ans.append(arr2[j])
+                res.append(arr2[j])
                 j+=1
-        ans.extend(arr1[i:])
-        ans.extend(arr2[j:])
+        res.extend(arr1[i:])
+        res.extend(arr2[j:])
         
         dummy=ListNode()
         curr=dummy
-        for num in ans:
+
+        for num in res:
             curr.next=ListNode(num)
             curr=curr.next
         return dummy.next
+
             
 
         
