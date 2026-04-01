afec79cd8a1442d6f3b89af06654260968ed2883
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-04-02 00:20:58 +0300

    Add solution for H-Index II

--- leetcode/0275-h-index-ii/0275-h-index-ii.py
+++ leetcode/0275-h-index-ii/0275-h-index-ii.py
@@ -1,15 +1,11 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        left = 0
-        right = len(citations) - 1
+        i=0
+        while i < (len(citations)):
+            while i < len(citations) and citations[i] < len(citations) - i:
+                i+=1
+            return len(citations) -i
+                
 
-        while left <= right:
-            mid =(left + right) // 2
-
-            if citations[mid] >= len(citations) - mid:
-                right = mid - 1
-            else:
-                left = mid + 1
-        return len(citations) - left
             
         
