dc6b3839e4e13f499794e0c9cc740cf80b41ef10
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-04-10 18:23:19 +0300

    Fix formatting of string concatenation in separateDigits

--- 2553. Separate the Digits in an Array.py
+++ 2553. Separate the Digits in an Array.py
@@ -2,6 +2,6 @@ class Solution:
     def separateDigits(self, nums: List[int]) -> List[int]:
         app=""
         for num in nums:
-            app+=str(num)
+            app+=str(num) 
         return [int(ch) for ch in app]
         
