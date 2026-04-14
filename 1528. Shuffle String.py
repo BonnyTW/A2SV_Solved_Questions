a20ce31ee11170f836fc4cc6562e90b6028203c0
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-04-14 18:38:29 +0300

    Correct string initialization and print output
    Fix string initialization and update print statement.

--- 1528. Shuffle String.py
+++ 1528. Shuffle String.py
@@ -2,7 +2,7 @@ class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         paired=list(sorted(zip((n for n in indices),(ch for ch in s))))
         print(paired)
-        ans=""
+        ans=''
         for p in paired:
             ans+=p[1]
         return ans
