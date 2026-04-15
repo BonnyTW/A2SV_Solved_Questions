5d237f3b59f41e03bccba5a8a320461d95192810
Bonny TW <155886730+BonnyTW@users.noreply.github.com>
2026-04-15 18:10:30 +0300

    Add solution for Set Mismatch

---
+++ leetcode/0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        count = Counter(nums)
+        dif = set(nums)
+        max_n = max(nums)
+
+        i = 1
+        miss = 0
+        dup = 0
+        while i <= max_n:
+            if i not in dif:
+                miss = i
+                break
+            i += 1
+        for num in nums:
+            count[num] -= 1
+            if count[num] == 1:
+                dup = num
+        if miss == 0:
+            miss = i
+        return [dup,miss]
+
+
+        
