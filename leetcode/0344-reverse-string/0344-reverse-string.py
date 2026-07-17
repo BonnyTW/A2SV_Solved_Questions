df2e8e32ccb0db34b585607a6af24b66acd649c4
Bonny Tilahun Wondimu <155886730+BonnyTW@users.noreply.github.com>
2026-07-17 17:44:22 +0300

    Refactor reverseString with iterative and recursive methods
    Refactor reverseString method to include both iterative and recursive approaches for reversing the string in-place.

--- leetcode/0344-reverse-string/0344-reverse-string.py
+++ leetcode/0344-reverse-string/0344-reverse-string.py
@@ -1,18 +1,23 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        
-        def helper(l,r):
-            if l>=r:
-                return
-            s[l],s[r]=s[r],s[l]
-            helper(l+1,r-1)
-            
-        helper(0,len(s)-1)
-        
+        # Iterative
+        '''
+        i = 0
+        j = len(s) - 1
 
+        while i <= j:
+            s[i],s[j] = s[j],s[i]
+            i += 1
+            j -= 1
+        '''
+
+        # Recursive
+        def helper(left,right):
+            if left >= right:
+                return
             
-        
+            s[left],s[right] = s[right],s[left]
+            helper(left + 1,right - 1)
+
+        helper(0,len(s) - 1)
         
