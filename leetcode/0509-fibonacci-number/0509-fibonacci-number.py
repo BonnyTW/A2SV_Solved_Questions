6253856c86ba026ec74fc672b8f99bb8ccc6da2b
Bonny Tilahun Wondimu <155886730+BonnyTW@users.noreply.github.com>
2026-07-17 17:12:48 +0300

    Refactor Fibonacci calculation method
    Refactor Fibonacci function to use a nested function for clarity.

--- leetcode/0509-fibonacci-number/0509-fibonacci-number.py
+++ leetcode/0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,10 +1,13 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n==0:
-            return 0
-        if n==1:
-            return 1
-        return self.fib(n-1)+self.fib(n-2)
+        def fib(n):
+            if n == 0:
+                return 0
+            if n == 1:
+                return 1
+            
+            return fib(n-1) + fib(n-2)
         
+        return fib(n)
 
         
