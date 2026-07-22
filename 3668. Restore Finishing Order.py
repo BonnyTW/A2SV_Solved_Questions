fcaf685fdece230ce9a7288ace90f1fd6576a028
Bonny Tilahun Wondimu <155886730+BonnyTW@users.noreply.github.com>
2026-07-22 13:40:12 +0300

    Create 3668. Restore Finishing Order.py

---
+++ 3668. Restore Finishing Order.py
@@ -0,0 +1,10 @@
+class Solution:
+    def recoverOrder(self, order: List[int], friends: List[int]) -> List[int]:
+        friends = set(friends)
+
+        ans = []
+        for num in order:
+            if num in friends:
+                ans.append(num)
+        
+        return ans
